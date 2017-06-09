3f16f71bc4c779d9663cac16a42a3be23146c01a
sebastianaf <sebastian.afanador@correounivalle.edu.co>
2017-06-09 00:07:03 -0500

    respues observacion
    Creo que los servidores deberian funcionar de manera conjunta en un solo servidor

--- Casino.java
+++ Casino.java
@@ -3,7 +3,7 @@ package casino_virtual;
 import java.net.ServerSocket;
 
 public class Casino { //servidor global //tengo la duda de si el hay que hacer dos clientes y dos sv, o hacer un sv central y luego dos sv secundarios
-	
+//SEBASTIAN: Yo opino que debemos hacer un solo servidor principal que maneje la logica de cualquiera de los 2 juegos	
 	private ServerSocket svSocket;
 	private Bacarat bacaratGame; // For Bacarat game
 	
